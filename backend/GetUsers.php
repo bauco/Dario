c61411bef36b8cdb20d38a541fe651acdf116fc1
bar <cohen.bau@gmail.com>
2023-03-28 19:36:21 +0300

    readme location

--- backend/GetUsers.php
+++ backend/GetUsers.php
@@ -4,7 +4,6 @@ require 'connect.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Headers: Content-Type");
 class UsersModel {
     private $pdo;
 
